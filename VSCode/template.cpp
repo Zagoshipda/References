7b97667a5a2579e9efe157a881ae080407bb3c63
JeonHyeong Lee <john713@hanmail.net>
2019-09-15 15:41:39 +0900

    Create template.cpp

---
+++ VSCode/template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+#define ll long long    // range : -9*10^18 ~ 9*10^18
+
+
+int main(){
+    IOS;
+
+    
+
+    return 0;
+}
